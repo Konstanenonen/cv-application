36faf60deac995d7bc3be7282d66d34ccc9d1ad1
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-04 00:03:09 +0300

    Added the basic logic for switching between form phase and ready cv phase on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from "react";
 
-function App() {
-  return <div>Hello</div>;
+class App extends React.Component<{}, { fillingForm: boolean }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      fillingForm: true,
+    };
+  }
+
+  render() {
+    const { fillingForm } = this.state;
+
+    return (
+      <div>{fillingForm ? <h1>Still on form</h1> : <h1>CV ready</h1>}</div>
+    );
+  }
 }
 
 export default App;
