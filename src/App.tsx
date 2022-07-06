87644ba14b2262043eda0857a0e96cf519555450
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-06 22:15:23 +0300

    Added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import CvGenerator from "./components/CvGenerator";
+import NavBar from "./components/NavBar";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <CvGenerator />
       </div>
     );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <h1>CV Generator</h1>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
