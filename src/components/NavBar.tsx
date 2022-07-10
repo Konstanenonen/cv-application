239dbff5fbcfb8d0bd49aa38fe61fbc5c528a380
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-10 15:57:33 +0300

    Made NavBar a functional component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,12 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import "../styles/NavBar.css";
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className="nav-bar">
-        <h1 className="nav-title">CV Generator</h1>
-      </nav>
-    );
-  }
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <h1 className="nav-title">CV Generator</h1>
+    </nav>
+  );
 }
 
 export default NavBar;
