4a691c883adc9e08426d3dd28adcb6f2e4397711
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-10 16:21:06 +0300

    Refactored Footer to be function component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,12 @@
 import React from "react";
 import "../styles/Footer.css";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <p className="footer-text">Made by Konsta Nenonen</p>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">Made by Konsta Nenonen</p>
+    </footer>
+  );
 }
 
 export default Footer;
