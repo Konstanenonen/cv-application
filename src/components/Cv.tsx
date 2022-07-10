ebe2023df43d0897003b9051584b1f5745f3175a
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-10 16:24:43 +0300

    Refactored Cv to be function component

--- src/components/Cv.tsx
+++ src/components/Cv.tsx
@@ -16,42 +16,38 @@ interface CvProps {
   workDuration: string;
 }
 
-class Cv extends React.Component<CvProps> {
-  render() {
-    const {
-      name,
-      email,
-      phoneNumber,
-      school,
-      titleOfStudy,
-      dateOfStudy,
-      work,
-      title,
-      tasks,
-      workDuration,
-    } = this.props;
-
-    return (
-      <div className="cv">
-        <Group name="Contact information">
-          <p>Name: {name}</p>
-          <p>Email: {email}</p>
-          <p>Phone number: {phoneNumber}</p>
-        </Group>
-        <Group name="Education">
-          <p>School name: {school}</p>
-          <p>Degree title: {titleOfStudy}</p>
-          <p>Duration of study: {dateOfStudy}</p>
-        </Group>
-        <Group name="Work Experience">
-          <p>Company name: {work}</p>
-          <p>Work title: {title}</p>
-          <p>Main tasks: {tasks}</p>
-          <p>Duration of work: {workDuration}</p>
-        </Group>
-      </div>
-    );
-  }
+function Cv({
+  name,
+  email,
+  phoneNumber,
+  school,
+  titleOfStudy,
+  dateOfStudy,
+  work,
+  title,
+  tasks,
+  workDuration,
+}: CvProps) {
+  return (
+    <div className="cv">
+      <Group name="Contact information">
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone number: {phoneNumber}</p>
+      </Group>
+      <Group name="Education">
+        <p>School name: {school}</p>
+        <p>Degree title: {titleOfStudy}</p>
+        <p>Duration of study: {dateOfStudy}</p>
+      </Group>
+      <Group name="Work Experience">
+        <p>Company name: {work}</p>
+        <p>Work title: {title}</p>
+        <p>Main tasks: {tasks}</p>
+        <p>Duration of work: {workDuration}</p>
+      </Group>
+    </div>
+  );
 }
 
 export default Cv;
