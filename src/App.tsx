3f428e87779577e0ef63b4ee1218ed698893f87d
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-10 16:43:48 +0300

    Refactored App to be function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import CvGenerator from "./components/CvGenerator";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import "./styles/App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <NavBar />
-        <CvGenerator />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <CvGenerator />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
